ad4de06c28ee6fe7c7523f1da3b4e37efc683d47
Deekshitha Pedasingu <pedasingudeekshitha0@gmail.com>
2026-05-22 08:50:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+let completedCount = 0;
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <div>
+      <button onclick="completeTask(this)">✔</button>
+      <button onclick="deleteTask(this)">❌</button>
+    </div>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
+
+function completeTask(button) {
+  const li = button.parentElement.parentElement;
+
+  if (!li.classList.contains("completed")) {
+    li.classList.add("completed");
+    completedCount++;
+    document.getElementById("count").innerText = completedCount;
+  }
+}
+
+function deleteTask(button) {
+  const li = button.parentElement.parentElement;
+  li.remove();
+}
+
+let timeLeft = 1500;
+let timerRunning = false;
+
+function startTimer() {
+  if (timerRunning) return;
+
+  timerRunning = true;
+
+  const timer = setInterval(() => {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("time").innerText =
+      `${minutes}:${seconds}`;
+
+    timeLeft--;
+
+    if (timeLeft < 0) {
+      clearInterval(timer);
+      timerRunning = false;
+      alert("Focus Session Completed!");
+    }
+  }, 1000);
+}
